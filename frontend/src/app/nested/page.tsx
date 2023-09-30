78eea0d0d1cd08169939ce44052bc81e51254ab7
Kamil Budzik <kbudzik@tbscg.com>
2023-09-30 11:28:50 +0200

    init

---
+++ frontend/src/app/nested/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Nested() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <header>I am the fallen</header>
+            </div>
+        </main>
+    )
+}
